2cfb208b5b2a8deb5f3526a09e457cf4a2ccb681
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-02-07 23:51:34 +0600

    update json description

--- orchestrator/model/least_expression.go
+++ orchestrator/model/least_expression.go
@@ -4,7 +4,7 @@ type LeastExpression struct {
 	Number1      float64       `json:"number1"`
 	Number2      float64       `json:"number2"`
 	Operation    OperationType `json:"operation"`
-	IdExpression int           `json:"id_expression"`
+	IdExpression int           `json:"idExpression"`
 }
 
 func NewLeastExpression() *LeastExpression {
