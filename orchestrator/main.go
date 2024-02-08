204ce50c8bd58247934ad46dc7fcac1bee782024
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-02-08 19:21:54 +0600

    add logger

--- orchestrator/main.go
+++ orchestrator/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"orchestrator/config"
+	"os"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Print(cfg)
+	log := setupLogger()
 
-	//todo: init slog
+	log.Debug("Hello logger")
+	log.Debug("%+v", cfg)
 	//todo: init storage
 	//todo: init router
 	//todo: run server
+}
+
+func setupLogger() *slog.Logger {
+	var log = slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	return log
 }
