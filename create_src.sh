3a91e305b7e030fae4dc2c356a469be9dd53a45f
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-04-07 23:09:05 +0500

    fix go path

--- create_src.sh
+++ create_src.sh
@@ -1,10 +1,18 @@
 # The file must be run in the root directory of the project
 
 # path to project must not contain spaces
-mkdir src
-go env -w GOPATH="$(pwd)/src"
+
+if [ ! -d src ]; then
+  mkdir src
+fi
+
+OLD_GOPATH=$(go env GOPATH)
+
+export GOPATH="$(pwd)/src"
 
 cd api_server && go mod download && cd ..
 cd auth_service && go mod download && cd ..
 cd expression_solver && go mod download && cd ..
 cd internal && go mod download && cd ..
+
+export GOPATH="$OLD_GOPATH"
